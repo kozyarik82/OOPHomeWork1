5b52eab4837aa0d1c01549d50558f9d04ce527db
kozyar <kozyarik@gmail.com>
2022-10-18 01:51:56 +0200

    задачи решены

--- Human.java
+++ Human.java
@@ -1,2 +1,35 @@
 public class Human {
+    int yearOfBirth;
+    String name;
+    String town;
+    String job;
+
+    Human(int yearOfBirth, String name, String town, String job) {
+        if (name == null) {
+            this.name = "информация не указана";
+        } else {
+            this.name = name;
+        }
+        if (town == null) {
+            this.town = "информация не указана";
+        } else {
+            this.town = town;
+        }
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }
+        if (job == null) {
+            this.job = "информация не указана";
+        } else {
+            this.job = job;
+        }
+    }
+
+    void greetings() {
+        System.out.println(" Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился/лась в " + yearOfBirth + " году. Я работаю на должности " +  job + " . Будем знакомы! ");
+
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.util.concurrent.Callable;
+
 public class Main {
     public static void main(String[] args) {
+        Human maksim = new Human(1987,"Максим","Минск","бренд-менеджер");
+        Human anna = new Human(1993,"Аня","Москва",  "методист образовательных программ");
+        Human kate = new Human(1994,"Катя", "Калининград",  "продакт-менеджер");
+        Human artyom = new Human(1995,"Артем", "Москва",  "директор по развитию бизнеса");
+
+        maksim.greetings();
+        anna.greetings();
+        kate.greetings();
+        artyom.greetings();
+
+
 
     }
 }
